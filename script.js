df54ec2a374328161153efab7306df62fb4048b3
itzdoxa <153353785+itzdoxa@users.noreply.github.com>
2024-05-10 22:41:44 +0530

    added delete and sign change button

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ const equals = document.querySelector("#equals");
 const clear = document.querySelector("#clear");
 const display = document.querySelector("#display");
 const operationLog = document.querySelector("#operationLog");
+const del = document.querySelector(".delete");
+const changeSign = document.querySelector("#changeSign")
 
 function sum(a, b){
     return Number(a) + Number(b);
-   // or else  return a + b;  string: 7 + 8 = 78
 };
 function subtract(a, b){
     return a - b;
@@ -24,115 +25,106 @@ let num2;
 let optr;
 let displayNum;
 let digit;
-let optrvar;
 let click;
 let result;
+let backspace;
+
+function operate(x, operator, y){
+    if(operator == "+"){
+           result = sum(x,y);
+           console.log("result: " + result);
+           return Number(result.toFixed(8));                       
+    };
+    if(operator == "-"){
+           result = subtract(x,y);
+           console.log("result: " + result);
+           return Number(result.toFixed(8));
+    };
+    if(operator == "*"){
+           result = multiply(x,y);
+           console.log("result: " + result);
+           return Number(result.toFixed(8));
+    };
+    if(operator == "/"){
+           result = divide(x,y);
+           console.log("result: " + result);
+           return Number(result.toFixed(8));
+    };
+};
 
 keypad.addEventListener('click', function(){
     if(event.target.classList.contains("digit")) {
-        digit = event.target.textContent;
-        if(event.target.classList.contains("period") && display.textContent.includes(".")) 
-            return;
 
-        display.textContent += digit;
-        displayNum = display.textContent; 
-        console.log("no. selected on keypad: " + displayNum);
-        console.log("display number is a: " + typeof(displayNum));
-    };  
+            digit = event.target.textContent;
+            if(event.target.classList.contains("period") && display.textContent.includes(".")) return;
+
+            display.textContent += digit;
+            displayNum = display.textContent; 
+      };  
+});
+
+del.addEventListener('click', function(){
+    display.textContent = display.textContent.slice(0, -1); 
+});
+
+changeSign.addEventListener('click', function(){
+    display.textContent = Number(display.textContent) * (-1);
 });
 
 operations.addEventListener('click', function(){
-    click = 0;
-    num1 = display.textContent; // string
-    optr = event.target.textContent;
-    display.textContent = "";
-    displayNum = "";
-    console.log("num1: " + num1 + " is a: " + typeof(num1));
-    console.log("operation to perform: " + optr + typeof(optr));
+        click = 0;
+        num1 = display.textContent; 
+        console.log("num1: " +  num1);
+        optr = event.target.textContent;
+        display.textContent = ""
+        displayNum = "";
 });
 
-const toFixedWithoutZeros = (num) => Number.parseFloat(num.toFixed(4));
+const toFixedWithoutZeros = (num) => Number.parseFloat(num.toFixed(8));
 
 let repeatValue;
-equals.addEventListener('click', function(){      
-    click++;
-    console.log("click count" + click)
 
-    if(click > 1) { 
+equals.addEventListener('click', function() {
+    click++;
+    console.log("same operation count: " + click);
+
+    if (click > 1) { 
         num1 = result;
-     //   display.textContent = toFixedWithoutZeros(Number(operate(num1, optr, repeatValue)));
-     display.textContent = toFixedWithoutZeros(operate(num1, optr, repeatValue));
- 
-      // let x = Number.isInteger(num1) ? num1.toString() : parseFloat(num1).toFixed(2);
-      // let y = Number.isInteger(repeatValue) ? repeatValue.toString() : parseFloat(repeatValue).toFixed(2);
-       let xnum1 = toFixedWithoutZeros(num1);
-       let yvalue = toFixedWithoutZeros(repeatValue);
-        operationLog.textContent = xnum1 + " " + optr + " " + yvalue; // i dont want this to overflow the calculator display,
-                                                      // so i have to use less decimal places
+        display.textContent = toFixedWithoutZeros(operate(num1, optr, repeatValue));
+
+        let fixNum1 = toFixedWithoutZeros(num1);
+        let fixRepeatValue = toFixedWithoutZeros(repeatValue);
+
+        operationLog.textContent = fixNum1 + " " + optr + " " + fixRepeatValue;  
         return;
 
     } else {
-        let num2 = display.textContent; // string
+        let num2 = display.textContent; 
+
         if (!num2) {
             if (optr === "-" || optr === "+") {
                 num2 = 0;
             } else if (optr === "*" || optr === "/") {
                 num2 = 1;
             }
-        }
-        repeatValue = Number(num2);  // number
+        };
+        console.log("num2: " + num2);
 
-        console.log("num2 " + num2 + " is a: " + typeof(num2));
-   
-     //   display.textContent = toFixedWithoutZeros(Number(operate(Number(num1), optr, Number(num2))));
+        repeatValue = Number(num2);  
         display.textContent = toFixedWithoutZeros(operate(num1, optr, num2));
-
-        console.log(display.textContent + ' display no. is ' + typeof(display.textContent))
-        console.log("num1: " + num1 + "; " + "num2: " + num2);
-
         operationLog.textContent = num1 + " " + optr + " " + num2;
     }
 });
 
 clear.addEventListener('click', function(){
-      click = 0;
-      num1 = "";
-      num2 = "";
-      optr = "";
-      display.textContent = "";
-      operationLog.textContent = "";
+         click = 0;
+         num1 = "";
+         num2 = "";
+         optr = "";
+         display.textContent = "";
+         operationLog.textContent = "";
 });
 
-function operate(x, operator, y){
-    if(operator == "+"){
-        console.log('function call + here x and y are ' + typeof(x) + typeof(y)); 
-        result = sum(x,y);
-        console.log(result + " result " + typeof(result)); // result will be a string
-        return Number(result.toFixed(4)); // to fixed returns a string to operation fxn, 
-                                  // that will further be added as text content to display
-    };
-    if(operator == "-"){
-        result = subtract(x,y);
-        console.log(result);
-        return Number(result.toFixed(4));
-    };
-    if(operator == "*"){
-        result = multiply(x,y);
-        console.log(result);
-        return Number(result.toFixed(4));
-    };
-    if(operator == "/"){
-        result = divide(x,y);
-        console.log("function call / here result is "+ typeof(result))
-        console.log(result);
-        return Number(result.toFixed(4));
-    };
-};
 
-// add feature: allow user to input a big expresion 
-// like in the google calc rather than a pair of numbers.
 
-// when the user picks num1 and opt and then presses equals without selecting
-// a second number,
-//make the result the same (+-0 and */1)
-// or dont do any operation
+
