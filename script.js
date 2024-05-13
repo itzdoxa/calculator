a8368730cb5cd0f9279f6e84e8856c72627689f6
itzdoxa <153353785+itzdoxa@users.noreply.github.com>
2024-05-13 14:02:59 +0530

    final commit, added ui

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const equals = document.querySelector("#equals");
 const clear = document.querySelector("#clear");
 const display = document.querySelector("#display");
 const operationLog = document.querySelector("#operationLog");
-const del = document.querySelector(".delete");
+const del = document.querySelector("#delete");
 const changeSign = document.querySelector("#changeSign")
 
 function sum(a, b){
@@ -33,27 +33,32 @@ function operate(x, operator, y){
     if(operator == "+"){
            result = sum(x,y);
            console.log("result: " + result);
-           return Number(result.toFixed(8));                       
+           return Number(result);
+        //   return Number(result.toFixed(8));                       
     };
     if(operator == "-"){
            result = subtract(x,y);
            console.log("result: " + result);
-           return Number(result.toFixed(8));
+           return Number(result);
+         //  return Number(result.toFixed(8));
+
     };
     if(operator == "*"){
            result = multiply(x,y);
            console.log("result: " + result);
-           return Number(result.toFixed(8));
+           return Number(result);
+        // return Number(result.toFixed(8));
     };
     if(operator == "/"){
            result = divide(x,y);
            console.log("result: " + result);
-           return Number(result.toFixed(8));
+           return Number(result);
+         //  return Number(result.toFixed(8));
     };
 };
 
 keypad.addEventListener('click', function(){
-    if(event.target.classList.contains("digit")) {
+    if(event.target.classList.contains("digit") && display.textContent.length < 32) {
 
             digit = event.target.textContent;
             if(event.target.classList.contains("period") && display.textContent.includes(".")) return;
@@ -72,12 +77,14 @@ changeSign.addEventListener('click', function(){
 });
 
 operations.addEventListener('click', function(){
+    if(event.target.classList.contains("operator")){
         click = 0;
         num1 = display.textContent; 
         console.log("num1: " +  num1);
         optr = event.target.textContent;
         display.textContent = ""
         displayNum = "";
+    }
 });
 
 const toFixedWithoutZeros = (num) => Number.parseFloat(num.toFixed(8));
@@ -90,10 +97,13 @@ equals.addEventListener('click', function() {
 
     if (click > 1) { 
         num1 = result;
-        display.textContent = toFixedWithoutZeros(operate(num1, optr, repeatValue));
+      //  display.textContent = toFixedWithoutZeros(operate(num1, optr, repeatValue));
+      display.textContent = (operate(num1, optr, repeatValue));
 
-        let fixNum1 = toFixedWithoutZeros(num1);
-        let fixRepeatValue = toFixedWithoutZeros(repeatValue);
+        // let fixNum1 = toFixedWithoutZeros(num1);
+        // let fixRepeatValue = toFixedWithoutZeros(repeatValue);
+        let fixNum1 = (num1);
+        let fixRepeatValue = (repeatValue);
 
         operationLog.textContent = fixNum1 + " " + optr + " " + fixRepeatValue;  
         return;
@@ -101,7 +111,7 @@ equals.addEventListener('click', function() {
     } else {
         let num2 = display.textContent; 
 
-        if (!num2) {
+        if (!num2) { // when user does not input second operand
             if (optr === "-" || optr === "+") {
                 num2 = 0;
             } else if (optr === "*" || optr === "/") {
@@ -111,7 +121,8 @@ equals.addEventListener('click', function() {
         console.log("num2: " + num2);
 
         repeatValue = Number(num2);  
-        display.textContent = toFixedWithoutZeros(operate(num1, optr, num2));
+       // display.textContent = toFixedWithoutZeros(operate(num1, optr, num2));
+       display.textContent = (operate(num1, optr, repeatValue));
         operationLog.textContent = num1 + " " + optr + " " + num2;
     }
 });
@@ -125,6 +136,11 @@ clear.addEventListener('click', function(){
          operationLog.textContent = "";
 });
 
-
+// add feature: allow user to input a long expression
+// fix: do not let the display overflow, i mean i can use string.length and then fix it 
+// but i do not want to limit the user from operating on big numbers
+// so it needs some thinking
+// the width of the display can contain around 32 characters
+// add error messages on the display if we limit the length of the number
 
 
